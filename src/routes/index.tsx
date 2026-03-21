bd3d143ec33b44f3791d13122b246f09a8cefc38
Sathvik Reddy Kolla <sathvikreddy8685@gmail.com>
2026-03-21 16:24:14 +0530

    feat: initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardHeader, CardTitle } from '#/components/ui/card'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome to the app</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>This is the home page</p>
+      </CardContent>
+    </Card>
+  )
+}
